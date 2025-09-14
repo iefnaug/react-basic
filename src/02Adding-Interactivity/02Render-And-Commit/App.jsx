e047e7a7c53cf14a2223d73dfeec29b47c4ab1f3
iefnaug <guanfei1993@gmail.com>
2025-09-14 19:16:09 +0800

    reader and commit

---
+++ src/02Adding-Interactivity/02Render-And-Commit/App.jsx
@@ -0,0 +1,20 @@
+function Image() {
+    return (
+        <img
+            src="https://i.imgur.com/ZF6s192.jpg"
+            alt="'Floralis Genérica' by Eduardo Catalano: a gigantic metallic flower sculpture with reflective petals"
+        />
+    )
+}
+
+export default function () {
+    console.info('call once...')
+    return (
+        <section>
+            <h1>Inspiring Sculptures</h1>
+            <Image></Image>
+            <Image></Image>
+            <Image></Image>
+        </section>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './02Adding-Interactivity/01Response-To-Events/App'
+import App from './02Adding-Interactivity/02Render-And-Commit/App'
+import {StrictMode} from "react";
 
 const root = createRoot(document.querySelector('#root'))
 
-root.render(<App />)
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+)
