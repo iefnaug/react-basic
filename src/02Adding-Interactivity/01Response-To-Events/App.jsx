174702a133e3ce42f84c615668c27a17bc9280f3
guanfei <guanfei1993@gmail.com>
2025-09-14 12:28:11 +0800

    阻止事件传播和默认行为

--- src/02Adding-Interactivity/01Response-To-Events/App.jsx
+++ src/02Adding-Interactivity/01Response-To-Events/App.jsx
@@ -13,17 +13,61 @@
 /**
  * Reading props in event handlers
  */
-function AlertButton(props) {
-    return (
-        <button onClick={() => alert(props.message)}>{props.children}</button>
-    )
-}
+// function AlertButton(props) {
+//     return (
+//         <button onClick={() => alert(props.message)}>{props.children}</button>
+//     )
+// }
+//
+// export default function Toolbar() {
+//     return (
+//         <div>
+//             <AlertButton message='Playing!'>Play Movie</AlertButton>
+//             <AlertButton message={'Uploading!'}>Upload Image</AlertButton>
+//         </div>
+//     )
+// }
 
-export default function Toolbar() {
+
+// function Button({onClick, children}) {
+//     return (
+//         <button onClick={e => {
+//             e.stopPropagation();
+//             onClick();
+//         }}>{children}</button>
+//     )
+// }
+
+/**
+ * 阻止事件传播
+ */
+// export default function Toolbar() {
+//     return (
+//         <div className="Toolbar" onClick={() => {
+//             alert('You clicked on the toolbar!');
+//             }}>
+//             <Button onClick={() => alert('Playing!')}>
+//                 Play Movie
+//             </Button>
+//             <Button onClick={() => alert('Uploading!')}>
+//                 Upload Image
+//             </Button>
+//         </div>
+//     )
+// }
+
+/**
+ * 阻止默认行为
+ */
+
+export default function Signup() {
     return (
-        <div>
-            <AlertButton message='Playing!'>Play Movie</AlertButton>
-            <AlertButton message={'Uploading!'}>Upload Image</AlertButton>
-        </div>
+        <form onSubmit={e => {
+            e.preventDefault();
+            alert(`Thanks for us!`);
+        }}>
+            <input type="text"/>
+            <button type="submit">Submit</button>
+        </form>
     )
 }
