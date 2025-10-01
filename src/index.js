497afd55e25a9311a1b7b01810e248a35b3b02f8
iefnaug <guanfei1993@gmail.com>
2025-10-01 17:09:42 +0800

    使用唯一key，让组件状态重置

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import './index.css'
 // import App from './02Adding-Interactivity/05-Updating-Object/App'
 // import App from './02Adding-Interactivity/06-Update-Arrays/App'
 // import App from './03Manage-State/01Reacting-To-Input-With-State/App'
-import App from './03Manage-State/03Sharing-State-Between-Components/App'
+// import App from './03Manage-State/03Sharing-State-Between-Components/App'
+import App from './03Manage-State/04Preserving-And-Resetting-State/App'
 // import {StrictMode} from "react";
 
 const root = createRoot(document.querySelector('#root'))
